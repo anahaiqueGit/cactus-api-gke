7ac7d4ce609bed480ad22d8189437b96f4ad5622
anahaiqueGit <anahaique.ai@gmail.com>
2026-03-01 18:17:22 -0300

    Agregado cluster GKE y node pool en Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -41,3 +41,46 @@ resource "google_sql_user" "cactus_user" {
   instance = google_sql_database_instance.cactus_db.name
   password = var.db_password
 }
+
+# Service Account para GKE
+resource "google_service_account" "gke_sa" {
+  account_id   = "cactus-gke-sa"
+  display_name = "Service Account para GKE"
+}
+
+# Cluster GKE
+resource "google_container_cluster" "cactus_cluster" {
+  name     = "cactus-cluster"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  service_account = google_service_account.gke_sa.email
+
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    network_policy_config {
+      disabled = true
+    }
+  }
+}
+
+# Node Pool para GKE
+resource "google_container_node_pool" "cactus_nodes" {
+  name       = "cactus-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.cactus_cluster.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    service_account = google_service_account.gke_sa.email
+    
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
